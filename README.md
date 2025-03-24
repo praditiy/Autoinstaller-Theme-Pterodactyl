047d17f5331b23c8312686f2398f1468753f8ede
Bangsano <sanoman984@gmail.com>
2025-03-24 21:08:09 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ bash <(curl -s https://raw.githubusercontent.com/Bangsano/Autoinstaller-Theme-Pt
 - Install Tema Stellar
 - Install Tema Billing
 - Install Tema Enigma
+- Install Tema Nightcore
 - Uninstall Tema
 
 ## System Support :
@@ -28,6 +29,21 @@ bash <(curl -s https://raw.githubusercontent.com/Bangsano/Autoinstaller-Theme-Pt
 |                  | 12      | :white_check_mark: |
 
 _\* di atas adalah os yang saya test, selebihnya bisa di test sendiri._
+
+# Pterodactyl Nightcore Theme
+---
+Screenshots:
+### Mainpage
+![Mainpage](https://raw.githubusercontent.com/Bangsano/Autoinstaller-Theme-Pterodactyl/main/images/Serverliste.png "Mainpage")
+### Login
+![Login](https://raw.githubusercontent.com/Bangsano/Autoinstaller-Theme-Pterodactyl/main/images/Login.png "Login")
+### Files
+![Files](https://raw.githubusercontent.com/Bangsano/Autoinstaller-Theme-Pterodactyl/main/images/Files.png "Files")
+### Editor
+![Editor](https://raw.githubusercontent.com/Bangsano/Autoinstaller-Theme-Pterodactyl/main/images/Editor.png "Editor")
+### Terminal
+![Terminal](https://raw.githubusercontent.com/Bangsano/Autoinstaller-Theme-Pterodactyl/main/images/Terminal.png "Terminal")
+
 
 ## Credits 
 - Github: [ SANO OFFICIAL ](https://github.com/Bangsano)
--- install.sh
+++ install.sh
@@ -5,8 +5,10 @@ BLUE='\033[0;34m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+RESET='\033[0m'
 NC='\033[0m'
-
 # Display welcome message
 
 #Update and install jq
@@ -56,6 +58,68 @@ check_token() {
   clear
 }
 
+# Install tema nightcore
+installThemeNightCore(){
+    echo -e "${GREEN}Installing ${YELLOW}sudo${GREEN} if not installed${RESET}"
+    apt install sudo -y > /dev/null 2>&1
+    cd /var/www/ > /dev/null 2>&1
+    echo -e "${GREEN}Unpack the themebackup...${RESET}"
+    tar -cvf Pterodactyl_Nightcore_Themebackup.tar.gz pterodactyl > /dev/null 2>&1
+    echo -e "${GREEN}Installing theme... ${RESET}"
+    cd /var/www/pterodactyl > /dev/null 2>&1
+    echo -e "${GREEN}Removing old theme if exist${RESET}"
+    rm -r Pterodactyl_Nightcore_Theme > /dev/null 2>&1
+    echo -e "${GREEN}Download the Theme${RESET}"
+    git clone https://github.com/Bangsano/Autoinstaller-Theme-Pterodactyl.git > /dev/null 2>&1
+    cd Pterodactyl_Nightcore_Theme > /dev/null 2>&1
+    echo -e "${GREEN}Removing old theme resources if exist${RESET}"
+    rm /var/www/pterodactyl/resources/scripts/Pterodactyl_Nightcore_Theme.css > /dev/null 2>&1
+    rm /var/www/pterodactyl/resources/scripts/index.tsx > /dev/null 2>&1
+    echo -e "${GREEN}Moving the new theme files to directory${RESET}"
+    mv index.tsx /var/www/pterodactyl/resources/scripts/index.tsx > /dev/null 2>&1
+    mv Pterodactyl_Nightcore_Theme.css /var/www/pterodactyl/resources/scripts/Pterodactyl_Nightcore_Theme.css > /dev/null 2>&1
+    cd /var/www/pterodactyl > /dev/null 2>&1
+    
+    curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash - > /dev/null 2>&1
+    apt update -y > /dev/null 2>&1
+    apt install nodejs -y > /dev/null 2>&1
+    
+    NODE_VERSION=$(node -v)
+    REQUIRED_VERSION="v16.20.2"
+    if [ "$NODE_VERSION" != "$REQUIRED_VERSION" ]; then
+        echo -e "${GREEN}Node.js version is not ${YELLOW}${REQUIRED_VERSION}${GREEN}. Version: ${YELLOW}${NODE_VERSION}${RESET}"
+        echo -e "${GREEN}Set version to ${YELLOW}v16.20.2${GREEN}... ${RESET}"
+        sudo npm install -g n > /dev/null 2>&1
+        sudo n 16 > /dev/null 2>&1
+        node -v > /dev/null 2>&1
+        npm -v > /dev/null  2>&1
+        echo -e "${GREEN}Now the default version is ${YELLOW}${REQUIRED_VERSION}"
+    else
+        echo -e "${GREEN}Node.js Version is compatible: ${YELLOW}${NODE_VERSION} ${RESET}"
+    fi
+
+    apt install npm -y > /dev/null 2>&1
+    npm i -g yarn > /dev/null 2>&1
+    yarn > /dev/null 2>&1
+
+    cd /var/www/pterodactyl > /dev/null 2>&1
+    echo -e "${GREEN}Rebuilding the Panel...${RESET}"
+    yarn build:production > /dev/null 2>&1
+    echo -e "${GREEN}Optimizing the Panel...${RESET}"
+    sudo php artisan optimize:clear > /dev/null 2>&1
+}
+
+installThemeQuestion(){
+    while true; do
+        read -p "Are you sure that you want to install the theme [y/n]? " yn
+        case $yn in
+            [Yy]* ) installThemeNightCore; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
 # Install theme
 install_theme() {
   while true; do
@@ -68,8 +132,9 @@ install_theme() {
     echo "1. stellar"
     echo "2. billing"
     echo "3. enigma"
+    echo "4. nightcore"
     echo "x. kembali"
-    echo -e "masukkan pilihan (1/2/3/x) :"
+    echo -e "masukkan pilihan (1/2/3/4/x) :"
     read -r SELECT_THEME
     case "$SELECT_THEME" in
       1)
@@ -84,6 +149,10 @@ install_theme() {
         THEME_URL=$(echo -e "https://github.com/Bangsano/Autoinstaller-Theme-Pterodactyl/raw/main/enigma.zip")
         break
         ;; 
+      4)
+        installThemeQuestion
+        break
+        ;;
       x)
         return
         ;;
